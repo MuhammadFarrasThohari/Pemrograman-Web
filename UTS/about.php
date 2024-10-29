fd79092cba3a8450c381e10f1f37705cf60e29a3
MuhammadFarrasThohari <farras.thohari@gmail.com>
2024-10-29 10:05:34 +0700

    Add: UTS Final Banget

--- UTS/about.php
+++ UTS/about.php
@@ -1,10 +1,16 @@
+<?php
+include "koneksi.php";
+$title = $profile["nama"];
+$about = $profile["about"];
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Muhammad Farras Thohari Ramadhan - About Me</title>
+    <title><?php echo $title ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -20,16 +26,12 @@
             <div class="row">
                 <div class="col-md-6 order-md-1 order-2 my-2">
                     <p>
-                        Saya adalah mahasiswa Informatika di Universitas Pembangunan Jaya. Saat ini saya sedang
-                        menjalani masa
-                        studi dan kini berada di semester 5. Saya memiliki ketertarikan di bidang web development dan
-                        juga
-                        embedded systems. Saya memiliki pengalaman dalam membuat aplikasi web menggunakan PHP, HTML,
-                        CSS, dan Javascript.
+                        <?php echo $about; ?>
                     </p>
                 </div>
                 <div class="col-md-6 order-md-2 order-1 d-flex justify-content-center my-2">
-                    <img src="https://via.placeholder.com/300" alt="Foto Profil" class="img-fluid rounded">
+                    <img src="./src/Profil.jpg" alt="Foto Profil" class="img-fluid rounded"
+                        style="height: 300px; width: 300px; object-fit: cover;">
                 </div>
             </div>
         </div>
