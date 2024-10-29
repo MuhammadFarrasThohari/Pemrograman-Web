a9fc413268823fd27609c5b5fcdfcf0f69e87819
MuhammadFarrasThohari <farras.thohari@gmail.com>
2024-10-29 10:05:50 +0700

    Add: UTS final final

--- UTS/koneksi.php
+++ UTS/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "123";
-$dbname = "uts_pemweb";
+$username = "muhamma6_pemweb";
+$password = "#Ayas1511";
+$dbname = "muhamma6_pemweb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
